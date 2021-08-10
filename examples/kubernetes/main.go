a0d4bab6b49e560bad1b290faee62a607703bbba
Alexander Liesenfeld <alexander.liesenfeld@outlook.com>
2021-08-10 15:50:39 +0200

    Rephrasing documentation.

--- examples/kubernetes/main.go
+++ examples/kubernetes/main.go
@@ -11,16 +11,19 @@ import (
 	"time"
 )
 
-// This is a an example configuration for Kubernetes liveness and readiness checks (for more info, please refer to
+// This is an example configuration that shows how Kubernetes liveness and readiness checks can be created with this
+// library (for more info, please visit
 // https://kubernetes.io/docs/tasks/configure-pod-container/configure-liveness-readiness-startup-probes/).
-// Please note that Kubernetes readiness and especially liveness checks need to be designed with care to not cause
+//
+// This file is accompanied by the file `example-pod-config.yaml` that is located in the same directory. It
+// contains a Kubernetes pod configuration that complements this implementation.
+//
+// Note that Kubernetes readiness and especially liveness checks need to be designed with care to not cause
 // any unintended behaviour (such as unexpected pod restarts, cascading failures, etc.). Please refer to the following
 // articles for guidance:
 // - https://www.innoq.com/en/blog/kubernetes-probes/
 // - https://blog.colinbreck.com/kubernetes-liveness-and-readiness-probes-how-to-avoid-shooting-yourself-in-the-foot/
 // - https://srcco.de/posts/kubernetes-liveness-probes-are-dangerous.html
-// Attention: Please see file `example-pod-config.yaml` in the same directory for an example configuration
-// that you can use to complement this check implementation example.
 func main() {
 	db, _ := sql.Open("sqlite3", "simple.sqlite")
 	defer db.Close()
@@ -32,11 +35,11 @@ func main() {
 		health.WithTimeout(10*time.Second),
 
 		// A check configuration to see if our database connection is up.
-		// Be wary though that this should be a "service private" database instance.
+		// Hint: Like with all external dependencies, this database instance is considered to be "service private".
 		// If many of your services use the same database instance, the readiness checks
-		// of all of these services will start failing on every small database hick-up.
-		// This is most likely not what you want. For guidance, please refer to the links
-		// listed in the main function documentation above.
+		// of all of these services will start failing at once on every database hick-up.
+		// This is most likely not what you want. For guidance on how to design Kubernetes checks,
+		// please refer to the links that are listed in the main function documentation above.
 		health.WithCheck(health.Check{
 			Name:  "database", // A unique check name.
 			Check: db.PingContext,
@@ -46,11 +49,9 @@ func main() {
 		// started with an initial delay of 3 seconds. The check function will NOT
 		// be executed for each HTTP request.
 		health.WithPeriodicCheck(15*time.Second, 3*time.Second, health.Check{
-			Name: "search",
-			// The check function checks the health of a component. If an error is
-			// returned, the component is considered unavailable ("down").
-			// The context contains a deadline according to the configuration of
-			// the Checker (global and .
+			Name: "disk",
+			// If the check function returns an error, this component will be considered unavailable ("down").
+			// The context contains a deadline according to the configuration of the Checker.
 			Check: func(ctx context.Context) error {
 				return fmt.Errorf("this makes the check fail")
 			},
@@ -60,7 +61,7 @@ func main() {
 		// More powerful hooks are also available (see docs). For guidance, please refer to the links
 		// listed in the main function documentation above.
 		health.WithStatusListener(func(ctx context.Context, state health.CheckerState) {
-			log.Println(fmt.Sprintf("health status changed to %s", state.Status))
+			log.Println(fmt.Sprintf("readiness status changed to %s", state.Status))
 		}),
 	)
 
