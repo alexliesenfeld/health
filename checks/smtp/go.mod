747870ec9a285dc6547e1bfd9e9053676cb6ca4f
Raphael Ebner <43086854+rafi0101@users.noreply.github.com>
2022-10-05 00:13:22 +0200

    Add SMTP server health check (#54)
    * ADD smtp check
    
    * CHG go.mod version and function name

---
+++ checks/smtp/go.mod
@@ -0,0 +1,5 @@
+module github.com/alexliesenfeld/health/checks/smtp
+
+go 1.13
+
+require github.com/stretchr/testify v1.8.0
---
+++ checks/smtp/smtp_test.go
@@ -0,0 +1,23 @@
+package smtp
+
+import (
+	"context"
+	"net/smtp"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestStatusUp(t *testing.T) {
+	// Arrange
+	client, err := smtp.Dial("localhost:25")
+	require.NoError(t, err)
+
+	check := New(client)
+
+	// Act
+	err = check(context.Background())
+
+	// Assert
+	require.NoError(t, err)
+}
