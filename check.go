27bc888bea6629dec6b571bd0f82d157224ad81b
Tom <tom.liefheid@ing.com>
2022-10-07 15:00:08 +0200

    split out availability

--- check.go
+++ check.go
@@ -114,34 +114,7 @@ type (
 	// InterceptorFunc is an interceptor function that intercepts any call to
 	// a components health check function.
 	InterceptorFunc func(ctx context.Context, name string, state CheckState) CheckState
-
-	// AvailabilityStatus expresses the availability of either
-	// a component or the whole system.
-	AvailabilityStatus string
 )
-
-const (
-	// StatusUnknown holds the information that the availability
-	// status is not known, because not all checks were executed yet.
-	StatusUnknown AvailabilityStatus = "unknown"
-	// StatusUp holds the information that the system or a component
-	// is up and running.
-	StatusUp AvailabilityStatus = "up"
-	// StatusDown holds the information that the system or a component
-	// down and not available.
-	StatusDown AvailabilityStatus = "down"
-)
-
-func (s AvailabilityStatus) criticality() int {
-	switch s {
-	case StatusDown:
-		return 2
-	case StatusUnknown:
-		return 1
-	default:
-		return 0
-	}
-}
 
 func newDefaultChecker(cfg checkerConfig) *defaultChecker {
 	checkState := map[string]CheckState{}
